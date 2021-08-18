9326dfd0971012586725168e0b956212acc26d42
Jay <jay@MacBook-Air-de-Jay.local>
2021-08-18 15:17:20 -0500

    seccion 17 auth vue-mitad

---
+++ Vue/Vue-The-Complete-Guide-Maximilian/16-find-a-coach/src/store/modules/auth/index.js
@@ -0,0 +1,17 @@
+import mutations from "./mutations.js";
+import getters from "./getters.js";
+import actions from "./actions.js";
+
+export default {
+  namespaced: false,
+  state() {
+    return {
+      userId: null,
+      token: null,
+      tokenExpiration: null
+    };
+  },
+  mutations,
+  getters,
+  actions
+}
