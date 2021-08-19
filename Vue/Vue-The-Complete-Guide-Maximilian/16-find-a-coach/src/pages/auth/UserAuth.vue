9fd8447be666262909ec7e69a7274410ab431a11
Jay <jsanchezr0328@gmail.com>
2021-08-19 18:11:25 -0500

    Finish section 17 auth in vue

--- Vue/Vue-The-Complete-Guide-Maximilian/16-find-a-coach/src/pages/auth/UserAuth.vue
+++ Vue/Vue-The-Complete-Guide-Maximilian/16-find-a-coach/src/pages/auth/UserAuth.vue
@@ -1,22 +1,30 @@
 <template>
-  <base-card>
-    <form @submit.prevent="submitForm">
-      <div class="form-control">
-        <label for="email">E-mail</label>
-        <input type="email" id="email" v-model.trim="email"/>
-      </div>
-      <div class="form-control">
-        <label for="password">Password</label>
-        <input type="password" id="password" v-model.trim="password"/>
-      </div>
-      <p
-        v-if="!formIsValid"
-        >Please enter a valid eail and password (must be at least 6 characters long)
-      </p>
-      <base-button>{{ submitButtonCaption }}</base-button>
-      <base-button type="button" mode="flat" @click="switchAuthMode" >{{ switchModeButtonCaption }}</base-button>
-    </form>
-  </base-card>
+  <div>
+    <base-dialog :show="!!error" title="An error occurred" @close="handleError">
+      <p>{{ error }}</p>
+    </base-dialog>
+    <base-dialog fixed :show="isLoading" title="Authenticating...">
+      <base-spinner></base-spinner>
+    </base-dialog>
+    <base-card>
+      <form @submit.prevent="submitForm">
+        <div class="form-control">
+          <label for="email">E-mail</label>
+          <input type="email" id="email" v-model.trim="email"/>
+        </div>
+        <div class="form-control">
+          <label for="password">Password</label>
+          <input type="password" id="password" v-model.trim="password"/>
+        </div>
+        <p
+          v-if="!formIsValid"
+          >Please enter a valid eail and password (must be at least 6 characters long)
+        </p>
+        <base-button>{{ submitButtonCaption }}</base-button>
+        <base-button type="button" mode="flat" @click="switchAuthMode" >{{ switchModeButtonCaption }}</base-button>
+      </form>
+    </base-card>
+  </div>
 </template>
 
 <script>
@@ -26,7 +34,9 @@ export default {
       email: '',
       password: '',
       formIsValid: true,
-      mode: 'login'
+      mode: 'login',
+      isLoading: false,
+      error: null
     }
   },
   computed: {
@@ -46,21 +56,33 @@ export default {
     }
   },
   methods: {
-    submitForm() {
+    async submitForm() {
       this.formIsValid = true;
       if (this.email === '' || !this.email.includes('@') || this.password.length < 6) {
         this.formIsValid = false;
         return;
       }
       
-      if (this.mode === 'login') {
-        //...
-      } else {
-        this.$store.dispatch('signup', {
-          email: this.email,
-          password: this.password
-        })
+      this.isloading = true;
+
+      const actionPayload = {
+        email: this.email,
+        password: this.password
       }
+
+      try {
+        if (this.mode === 'login') {
+          await this.$store.dispatch('login', actionPayload)
+        } else {
+          await this.$store.dispatch('signup', actionPayload)
+        }
+        const redirectUrl = '/' + (this.$route.query.redirect || 'coaches');
+        this.$router.replace(redirectUrl);
+      } catch (err) {
+        this.error = err.message || 'Failed to authenticate, try later';
+      }
+
+      this.isloading = false
     },
     switchAuthMode() {
       if (this.mode ==='login') {
@@ -68,6 +90,9 @@ export default {
       } else {
         this.mode = 'login'
       }
+    },
+    handleError() {
+      this.error = null
     }
   }
 }
